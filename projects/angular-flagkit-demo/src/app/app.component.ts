5f1b95c494c4431a4711a19d0b9523b87c97f01b
Andriy Horen <go.andriy@gmail.com>
2024-04-20 20:07:55 +0200

    feat: clean-up deafult angular html template

--- projects/angular-flagkit-demo/src/app/app.component.ts
+++ projects/angular-flagkit-demo/src/app/app.component.ts
@@ -1,24 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FlagComponent } from 'angular-flagkit';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FlagComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'Angular FlagKit Demo';
 
+  constructor() {}
+
   menuItems = [
-    { title: 'Explore the Docs', link: 'https://angular.dev' },
-    { title: 'Learn with Tutorials', link: 'https://angular.dev/tutorials' },
-    { title: 'CLI Docs', link: 'https://angular.dev/tools/cli' },
     {
-      title: 'Angular Language Service',
-      link: 'https://angular.dev/tools/language-service',
+      title: 'GitHub repo',
+      link: 'https://github.com/andriy-horen/angular-flagkit',
     },
-    { title: 'Angular DevTools', link: 'https://angular.dev/tools/devtools' },
+    {
+      title: 'NPM package',
+      link: 'https://www.npmjs.com/package/angular-flagkit',
+    },
+    {
+      title: 'FlagKit 2.0 by Bowtie',
+      link: 'https://github.com/madebybowtie/FlagKit',
+    },
   ];
 }
