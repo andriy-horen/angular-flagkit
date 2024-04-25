fae8fe93c092cc904c51a78ce73231fe4a077315
Andriy Horen <go.andriy@gmail.com>
2024-04-25 20:32:11 +0200

    feat: implement flag source directive

--- projects/angular-flagkit-demo/src/app/flag-comparison/flag-comparison.component.ts
+++ projects/angular-flagkit-demo/src/app/flag-comparison/flag-comparison.component.ts
@@ -1,30 +1,25 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import {
   FLAG_COUNTRY_ALPHA2_LOOKUP,
   FlagComponent,
   provideFlags,
 } from 'angular-flagkit';
 import * as flags from 'angular-flagkit/flags';
+import { FlagSrcDirective } from '../flag-src.directive';
 import { FlagsDataService } from '../flags-data.service';
 import { SvgImgComponent } from '../svg-img/svg-img.component';
 
 @Component({
   selector: 'app-flag-comparison',
   standalone: true,
-  imports: [FlagComponent, SvgImgComponent],
+  imports: [FlagComponent, SvgImgComponent, FlagSrcDirective],
   providers: [provideFlags({ ...flags })],
   templateUrl: './flag-comparison.component.html',
   styleUrl: './flag-comparison.component.scss',
 })
-export class FlagComparisonComponent implements OnInit {
+export class FlagComparisonComponent {
   readonly countries = inject(FlagsDataService).getIso3166Data();
   readonly flagLookup = inject<Record<string, string>>(
     FLAG_COUNTRY_ALPHA2_LOOKUP,
   );
-
-  ngOnInit(): void {}
-
-  buildSvgCountryFlagsUrl(alpha2: string) {
-    return `assets/svg-country-flags/${alpha2.toLowerCase()}.svg`;
-  }
 }
---
+++ projects/angular-flagkit-demo/src/app/flag-src.directive.spec.ts
@@ -0,0 +1,8 @@
+import { FlagSrcDirective } from './flag-src.directive';
+
+describe('FlagSrcDirective', () => {
+  it('should create an instance', () => {
+    const directive = new FlagSrcDirective();
+    expect(directive).toBeTruthy();
+  });
+});
